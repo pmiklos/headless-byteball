968b9d0b88d918f8d8f0fc6fc1fad00c59ef0607
Peter Miklos <pmiklos@sirokko.net>
2017-02-23 17:47:27 -0800

    changed to testnet

--- conf.js
+++ conf.js
@@ -10,7 +10,7 @@ exports.bLight = false;
 exports.storage = 'sqlite';
 
 
-exports.hub = 'byteball.org/bb';
+exports.hub = 'byteball.org/bb-test';
 exports.deviceName = 'Headless';
 exports.permanent_paring_secret = 'randomstring';
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
